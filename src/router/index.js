a8a87535e9cd7f86ec3d6d8563d5cbaf2b1cedbd
seoguhoe2 <js8337349@naver.com>
2025-10-23 16:14:33 +0900

    리뷰 게시물, 멘토링 게시물 완료

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,13 @@ import FindIdView from '../components/FindIdView.vue';
 import FindPasswordView from '../components/FindPasswordView.vue';
 import AdminLoginView from '../components/AdminLoginView.vue';
 import ChangePasswordView from '../components/ChangePasswordView.vue';
+import MentoringBoardView from '../components/post/mentoring/MentoringBoardView.vue';
+import RegistMentoringPostView from '../components/post/mentoring/RegistMentoringPostView.vue';
+import MentoringPostView from '../components/post/mentoring/MentoringPostView.vue';
+import ReviewBoardView from '../components/post/review/ReviewBoardView.vue';
+import RegistReviewPostView from '../components/post/review/RegistReviewPostView.vue';
+import ReviewPostView from '../components/post/review/ReviewPostView.vue';
+import FashionBoardView from '../components/post/fashion/FashionBoardView.vue';
 
 const routes = [
   {
@@ -36,7 +43,44 @@ const routes = [
     path: '/changepassword',
     name: 'changepassword',
     component: ChangePasswordView
-  }
+  }, {
+    path: '/mentoringboard',
+    name: 'mentoringboard',
+    component: MentoringBoardView
+  }, {
+    path: '/registmentoringpost',
+    name: 'registmentoringpost',
+    component: RegistMentoringPostView
+  }, {
+    path: '/mentoringpost',
+    name: 'mentoringpost',
+    component: MentoringPostView
+  }, {
+    path: '/reviewboard',
+    name: 'reviewboard',
+    component: ReviewBoardView
+  }, {
+    path: '/registreviewpost',
+    name: 'registreviewpost',
+    component: RegistReviewPostView
+  }, {
+    path: '/reviewpostview',
+    name: 'reviewpostview',
+    component: ReviewPostView
+  }, {
+    path: '/fashionboardview',
+    name: 'fashionboardview',
+    component: FashionBoardView
+  } 
+  // {
+  //   path: '/reviewpostview',
+  //   name: 'reviewpostview',
+  //   component: ReviewPostView
+  // }, {
+  //   path: '/reviewpostview',
+  //   name: 'reviewpostview',
+  //   component: ReviewPostView
+  // }
 ]
 
 const router = createRouter({
